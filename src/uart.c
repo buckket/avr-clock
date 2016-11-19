470edd7635685068b4c3557737dedc50f8cecbec
buckket <buckket@cock.li>
2016-11-20 00:00:36 +0100

    Add UART support

---
+++ src/uart.c
@@ -0,0 +1,30 @@
+#include "uart.h"
+
+#define BAUD 9600
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)
+
+void uart_init(void) {
+    // Set baud rate
+    UBRR0H = (BAUDRATE >> 8);
+    UBRR0L = (BAUDRATE & 0xFF);
+
+    // Enable transmitter
+    UCSR0B = (1 << TXEN0);
+
+    // Set default data format (8N1)
+    UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
+}
+
+void uart_transmit(uint8_t data) {
+    // Wait for empty transmit buffer
+    while (!(UCSR0A & (1 << UDRE0)));
+
+    // Put data into buffer, send the data
+    UDR0 = data;
+
+    // Wait till data transfer is complete
+    while (!(UCSR0A & (1 << TXC0)));
+
+    // Clear flag
+    UCSR0A |= (1 << TXC0);
+}
---
+++ src/uart.h
@@ -0,0 +1,5 @@
+#include <avr/io.h>
+
+void uart_init(void);
+
+void uart_transmit(uint8_t data);
